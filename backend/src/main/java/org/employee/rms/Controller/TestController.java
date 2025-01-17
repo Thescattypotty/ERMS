ada1c1f2644fd0a1f062b00098489220da33a385
Thescattypotty <bnsyahya123@gmail.com>
2025-01-17 14:40:49 +0100

    Finishing docs apis

--- backend/src/main/java/org/employee/rms/Controller/TestController.java
+++ backend/src/main/java/org/employee/rms/Controller/TestController.java
@@ -6,40 +6,57 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 @RestController
 @RequestMapping("/api/v1/test")
+@Tag(name = "Test", description = "Test Controller")
 public class TestController {
     
+    @Operation(summary = "Protected Route", description = "Protected Route")
+    @ApiResponse(responseCode = "200", description = "Protected Route")
     @GetMapping("/protected")
     @PreAuthorize("isAuthenticated()")
     public ResponseEntity<String> protectedRoute() {
         return ResponseEntity.ok("Test");
     }
 
+    @Operation(summary = "Protected Route Read", description = "Protected Route Read")
+    @ApiResponse(responseCode = "200", description = "Protected Route Read")
     @GetMapping("/protected/read")
     @PreAuthorize("hasAuthority('READ')")
     public ResponseEntity<String> protectedRouteRead() {
         return ResponseEntity.ok("Test");
     }
 
+    @Operation(summary = "Protected Route Create", description = "Protected Route Create")
+    @ApiResponse(responseCode = "200", description = "Protected Route Create")
     @GetMapping("/protected/create")
     @PreAuthorize("hasAuthority('CREATE')")
     public ResponseEntity<String> protectedRouteCreate() {
         return ResponseEntity.ok("Test");
     }
 
+    @Operation(summary = "Protected Route Update", description = "Protected Route Update")
+    @ApiResponse(responseCode = "200", description = "Protected Route Update")
     @GetMapping("/protected/update")
     @PreAuthorize("hasAuthority('UPDATE')")
     public ResponseEntity<String> protectedRouteUpdate() {
         return ResponseEntity.ok("Test");
     }
 
+    @Operation(summary = "Protected Route Delete", description = "Protected Route Delete")
+    @ApiResponse(responseCode = "200", description = "Protected Route Delete")
     @GetMapping("/protected/delete")
     @PreAuthorize("hasAuthority('DELETE')")
     public ResponseEntity<String> protectedRouteDelete() {
         return ResponseEntity.ok("Test");
     }
 
+    @Operation(summary = "Unprotected Route", description = "Unprotected Route")
+    @ApiResponse(responseCode = "200", description = "Unprotected Route")
     @GetMapping("/unprotected")
     public ResponseEntity<String> unprotectedRoute() {
         return ResponseEntity.ok("Test");
--- backend/src/main/java/org/employee/rms/Controller/UserController.java
+++ backend/src/main/java/org/employee/rms/Controller/UserController.java
@@ -18,16 +18,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/api/v1/user")
 @RequiredArgsConstructor
+@Tag(name = "User", description = "User API")
 public class UserController {
     
     private final UserService userService;
 
+    @Operation(summary = "Create User", description = "Create a new user", tags = { "User" })
+    @ApiResponse(responseCode = "201", description = "User created")
+    @Schema(name = "UserRequest", implementation = UserRequest.class)
     @PostMapping
     @PreAuthorize("hasAuthority('ROLE_ADMINISTRATOR')")
     public ResponseEntity<Void> createUser(@RequestBody @Valid UserRequest userRequest){
@@ -35,6 +43,9 @@ public class UserController {
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
+    @Operation(summary = "Get Users", description = "Get all users", tags = { "User" })
+    @ApiResponse(responseCode = "200", description = "Users fetched")
+    @Schema(name = "UserResponse", implementation = UserResponse.class)
     @GetMapping
     @PreAuthorize("hasAuthority('ROLE_ADMINISTRATOR')")
     public ResponseEntity<Page<UserResponse>> getUsers(
@@ -46,18 +57,27 @@ public class UserController {
         return ResponseEntity.ok(userService.getAllUsers(page, size, sortBy, direction));
     }
 
+    @Operation(summary = "Get User", description = "Get a user by id", tags = { "User" })
+    @ApiResponse(responseCode = "200", description = "User fetched")
+    @Schema(name = "UserResponse", implementation = UserResponse.class)
     @GetMapping("/{id}")
     @PreAuthorize("hasAuthority('ROLE_ADMINISTRATOR')")
     public ResponseEntity<UserResponse> getUser(@PathVariable("id") String id){
         return ResponseEntity.ok(userService.getUser(id));
     }
 
+    @Operation(summary = "Update User", description = "Update an existing user", tags = { "User" })
+    @ApiResponse(responseCode = "200", description = "User updated")
+    @Schema(name = "UserRequest", implementation = UserRequest.class)
     @PutMapping("/{id}")
     @PreAuthorize("hasAuthority('ROLE_ADMINISTRATOR')")
     public ResponseEntity<Void> changePassword(@PathVariable("id") String id, @RequestBody @Valid ChangePasswordRequest changePasswordRequest){
         userService.ChangePassword(id, changePasswordRequest);
         return new ResponseEntity<>(HttpStatus.OK);
     }
+
+    @Operation(summary = "Delete User", description = "Delete an existing user", tags = { "User" })
+    @ApiResponse(responseCode = "204", description = "User deleted")
     @DeleteMapping("/{id}")
     @PreAuthorize("hasAuthority('ROLE_ADMINISTRATOR')")
     public ResponseEntity<Void> deleteUser(@PathVariable("id") String id){
